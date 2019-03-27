db7e3ed6deeeb36bbeb8b98e694c6bf46dc71670
qinhp <qhp603560399@163.com>
2019-03-27 09:19:24 +0800

    Initial commit

---
+++ app/src/main/java/com/doing/qinhp/customizeview/TextView.java
@@ -0,0 +1,17 @@
+package com.doing.qinhp.customizeview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class TextView extends View {
+    public TextView(Context context) {
+        super(context);
+    }
+
+    public TextView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
