9f8c6dd380f79adfbd5073942d3602a984a27c45
qinhp <qhp603560399@163.com>
2019-03-28 10:22:14 +0800

    画了个圆

--- app/src/main/java/com/doing/qinhp/customizeview/TextView.java
+++ app/src/main/java/com/doing/qinhp/customizeview/TextView.java
@@ -1,17 +1,68 @@
 package com.doing.qinhp.customizeview;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Point;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
 import android.view.View;
+import android.view.WindowManager;
 
 import androidx.annotation.Nullable;
 
 public class TextView extends View {
+
+    private Point point;
+    private Paint paint;
+    Paint paint1;
+
+    private int width;
+    private int heigth;
+
     public TextView(Context context) {
         super(context);
     }
 
     public TextView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        paint = new Paint();
+        paint1 = new Paint();
+    }
+
+
+    private DisplayMetrics getXY() {
+        WindowManager windowManager = (WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        windowManager.getDefaultDisplay().getMetrics(displayMetrics);
+        return displayMetrics;
+
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        paint.setARGB(50, 0, 235, 235);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeCap(Paint.Cap.ROUND);
+        paint.setStrokeWidth(20);
+        canvas.drawCircle(getXY().widthPixels/2, getXY().heightPixels/2, 200, paint);
+        canvas.rotate(-90);
+
+        paint1.setARGB(50, 255, 0, 0);
+        paint1.setTextSize(50);
+        canvas.drawText("hello", 500, 500, paint1);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
     }
 }
